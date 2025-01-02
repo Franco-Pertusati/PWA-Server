1f2f0c63e9d797a940d945fa17d3fd90cbc274f5
Franco Pertusati <pertusatifranco836@gmail.com>
2025-01-02 15:51:43 -0300

    Configurado para usar en vercel

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-const express = require('express');
-const webPush = require('web-push');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const webPush = require("web-push");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
 
+// Claves VAPID generadas
 const vapidKeys = {
   publicKey: process.env.VAPID_PUBLIC_KEY,
   privateKey: process.env.VAPID_PRIVATE_KEY,
 };
 
+// Configurar las claves VAPID
+webPush.setVapidDetails(
+  "mailto:your-email@example.com",
+  publicVapidKey,
+  privateVapidKey
+);
+
 // Middleware
-app.use(cors({ origin: 'https://pwa-test-mcb4.vercel.app' }));
+app.use(cors({ origin: "https://pwa-test-mcb4.vercel.app" }));
 app.use(bodyParser.json());
 
 // Endpoint para probar el servidor
-app.get('/health', (req, res) => {
-  res.status(200).json({ message: 'Server is running!' });
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Server is running!" });
 });
 
 // Endpoint para registrar suscripciones
-const subscriptions = []; // Array para almacenar las suscripciones
+const subscriptions = [];
 
-app.post('/subscribe', (req, res) => {
+app.post("/subscribe", (req, res) => {
   const subscription = req.body;
   subscriptions.push(subscription);
-  res.status(201).json({ message: 'Subscription added successfully!' });
+  res.status(201).json({ message: "Subscription added successfully!" });
 });
 
-// Endpoint para enviar notificaciones push
-app.post('/send-notification', (req, res) => {
+app.post("/send-notification", (req, res) => {
   const { title, message } = req.body;
   const payload = JSON.stringify({ title, message });
 
-  // Enviar notificaciones a todas las suscripciones
   subscriptions.forEach((subscription) => {
     webPush
       .sendNotification(subscription, payload)
-      .catch((error) => console.error('Error sending notification:', error));
+      .catch((error) => console.error("Error sending notification:", error));
   });
 
-  res.status(200).json({ message: 'Notifications sent successfully!' });
+  res.status(200).json({ message: "Notifications sent successfully!" });
 });
 
-// Inicio del servidor
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// Exportar la aplicación
+module.exports = app;
